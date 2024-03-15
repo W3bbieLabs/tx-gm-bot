13268cf79aa2f55aee0319379bb7fceedf0eb929
Chris <chris35469@gmail.com>
2024-03-15 12:06:15 -0500

    Format mint correctly

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -40,14 +40,18 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let user_info = await getUserInfo(fid)
   let { username } = user_info
   let time = Date.now()
+
+  return gmFrame(username, address, time)
   //console.log(message)
 
   // User tapped refresh button
+  /*
   if (message?.button == 2) {
     return gmFrame(username, address, time)
-  }
+  }*/
 
   // No verified address found
+  /*
   if (address == '') return errorFrame("Please add a Verified Address")
   let contract_response = await airDrop(CONTRACT_ADDRESS, address, Zora1155ABI, TOKEN_ID)
   if (contract_response.status == ContractResponse.SUCCESS) {
@@ -56,6 +60,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   } else {
     return errorFrame("Opps! Something went wrong.")
   }
+  */
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -4,8 +4,8 @@ import { base } from 'viem/chains';
 import { encodeFunctionData, parseEther } from 'viem';
 //import BuyMeACoffeeABI from "../../utils/BuyMeACoffeeABI"
 import { Zora1155ABI } from "../../utils/Zora1155"
-import { GM_CONTRACT, Referral } from '../../config'
-//import { toBytes } from 'viem'
+import { GM_CONTRACT, Referral, MINTER } from '../../config'
+import { getAbiItem, Hex, toBytes, hexToNumber } from 'viem'
 
 let validateParam = (param: string, request: Request) => {
     const { searchParams } = new URL(request.url);
@@ -16,29 +16,45 @@ let validateParam = (param: string, request: Request) => {
 
 async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
     const body: FrameRequest = await req.json();
-    const address: `0x${string}` = validateParam('address', req) as `0x${string}`
-    const { isValid } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+    //const address: `0x${string}` = validateParam('address', req) as `0x${string}`
+    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+    let address = message ? message.interactor.verified_accounts[0] as Hex : '' as Hex
 
+    //console.log(message)
     if (!isValid) {
         return new NextResponse('Message not valid', { status: 500 });
     }
 
-    //const mintTo = toBytes(address)
+    //'0x0000000000000000000000004cb7fa20eb4007506cd4196b8a399b03669ca54a'
+    //let temp_address = "0x4Cb7FA20eB4007506cD4196B8a399b03669Ca54a" as Hex
+    let new_address = `0x000000000000000000000000${address.split('0x')[1]}` as Hex
+    let _ref = '0x0000000000000000000000000000000000000000' as Hex
+    //let _minter = '0x04E2516A2c207E84a1839755675dfd8eF6302F0a' as Hex
+    //let _to = '0x7777777f279eba3d3ad8f4e708545291a6fdba8b'
+    //let mintTo = hexToNumber(temp_address)
+    //let minTo: Hex = mintTo as Hex
+    console.log("address", address)
 
     const data = encodeFunctionData({
         abi: Zora1155ABI,
         functionName: 'mintWithRewards',
-        args: [GM_CONTRACT, parseEther('1'), parseEther('1'), address, Referral],
+        args: [MINTER, 1n, 1n, new_address, _ref],
     });
 
-    const txData: FrameTransactionResponse = {
+    const functionAbi = getAbiItem({
+        abi: Zora1155ABI,
+        name: "mintWithRewards",
+    })
+
+    //const errorsAbi = Zora1155ABI.filter((t) => t.type === "error");
+    const txData = {
         chainId: `eip155:${base.id}`, // Remember Base Sepolia might not work on Warpcast yet
         method: 'eth_sendTransaction',
         params: {
-            abi: [],
+            abi: [functionAbi],
             data,
             to: GM_CONTRACT,
-            value: parseEther('0').toString(), // 0.00004 ETH
+            value: parseEther('0.000777').toString(), // 0.00004 ETH
         },
     };
 
